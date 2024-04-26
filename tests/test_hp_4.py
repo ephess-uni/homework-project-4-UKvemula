a279c5427051c2dd6cd69f09b1135f377d0e30b5
UKvemula <uvemulap@cbu.edu>
2024-04-26 03:12:26 -0400

    k

--- tests/test_hp_4.py
+++ tests/test_hp_4.py
@@ -13,12 +13,15 @@ from src.hp_4 import (
 
 TEMP_DIR = TemporaryDirectory()
 
+
 @pytest.fixture
 def temp_dir():
     return TEMP_DIR.name
 
+
 argument_fixture = ['2000-10-01', '2000-10-02', '2000-10-03']
 expected_fixture = ['01 Oct 2000', '02 Oct 2000', '03 Oct 2000']
+
 
 @pytest.mark.parametrize(
     'arg,expected',
@@ -29,18 +32,22 @@ expected_fixture = ['01 Oct 2000', '02 Oct 2000', '03 Oct 2000']
 def test___reformat_dates___should_correctly_reformat(arg, expected):
     assert sorted(reformat_dates(arg)) == sorted(expected)
 
+
 def test___date_range___should_return_list_of_datetime_objects():
     actual = date_range('2000-01-01', 3)
     assert isinstance(actual, list)
     assert isinstance(actual[0], datetime)
 
+
 def test___date_range___raises_type_error_for_start():
     with pytest.raises(TypeError):
         date_range(datetime(2000, 1, 1), 3)
 
+
 def test___date_range___raises_type_error_for_n():
     with pytest.raises(TypeError):
         date_range('2000-01-01', '3')
+
 
 def test___date_range___returns_correct_values():
     actual = date_range('2000-01-01', 3)
@@ -50,6 +57,7 @@ def test___date_range___returns_correct_values():
         datetime(2000, 1, 3),
     ]
     assert actual == expected
+
 
 def test___add_date_range___returns_correct_values_input_1():
     values = [1, 2, 3]
@@ -62,6 +70,7 @@ def test___add_date_range___returns_correct_values_input_1():
     expected = list(zip(expected_dates, values))
     assert add_date_range(values, start_date) == expected
 
+
 def test___add_date_range___returns_correct_values_input_2():
     values = [11, 12, 13]
     start_date = '2001-01-31'
@@ -73,13 +82,16 @@ def test___add_date_range___returns_correct_values_input_2():
     expected = list(zip(expected_dates, values))
     assert add_date_range(values, start_date) == expected
 
+
 @pytest.fixture
 def book_returns_short():
     return 'tests/fixtures/book_returns_short.csv'
 
+
 @pytest.fixture
 def book_returns():
     return 'tests/fixtures/book_returns.csv'
+
 
 @pytest.fixture
 def fees_report_out_short(book_returns_short, temp_dir):
@@ -92,10 +104,11 @@ def fees_report_out_short(book_returns_short, temp_dir):
         outfile
     )
     with open(outfile) as f:
-       reader = DictReader(f)
-       rows = [row for row in reader]
+        reader = DictReader(f)
+        rows = [row for row in reader]
 
     return rows
+
 
 @pytest.fixture
 def fees_report_out(book_returns, temp_dir):
@@ -108,20 +121,23 @@ def fees_report_out(book_returns, temp_dir):
         outfile
     )
     with open(outfile) as f:
-       reader = DictReader(f)
-       rows = [row for row in reader]
+        reader = DictReader(f)
+        rows = [row for row in reader]
 
     return rows
+
 
 def test___fees_report___has_correct_fieldnames(fees_report_out_short):
     assert 'patron_id' in fees_report_out_short[0].keys()
     assert 'late_fees' in fees_report_out_short[0].keys()
+
 
 def test___fees_report___has_correct_currency_format(fees_report_out_short):
     fees = [row['late_fees'] for row in fees_report_out_short]
     assert all('$' not in fee for fee in fees)
     assert all('.' in fee for fee in fees)
     assert all(len(fee.split('.')[-1]) == 2 for fee in fees)
+
 
 def test___fees_report___includes_all_patrons(fees_report_out_short):
     expected_fees = {
@@ -133,17 +149,15 @@ def test___fees_report___includes_all_patrons(fees_report_out_short):
     assert sorted(actual_patrons) == sorted(expected_patrons)
 
 
-
 def test___fees_report___has_correct_fees(fees_report_out_short):
     expected_fees = {
         '17-873-8783': '15.00',
         '83-279-0036': '0.00'
     }
-    assert all(
-        fee['late_fees'] == expected_fees[fee['patron_id']]
-        for fee in fees_report_out_short
-    )
+    for row in fees_report_out_short:
+        assert row['late_fees'] == expected_fees[row['patron_id']]
 
-def test___fees_report___has_one_row_per_patron(fees_report_out):
-    patrons = Counter(fee['patron_id'] for fee in fees_report_out)
-    assert all(val == 1 for val in patrons.values())
+
+def test___fees_report___has_one_row_per_patron(fees_report_out_short):
+    patron_ids = [row['patron_id'] for row in fees_report_out_short]
+    assert len(patron_ids) == len(set(patron_ids))
